c90f48da1ee653ad2c55c51404aa028d69633f6e
Marco <minucci9292@gmail.com>
2020-10-14 16:06:18 +0200

    Initial commit
    -

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float power;
+
+    public Text points;
+
+    int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        power = 1;
+
+        counter = 0;
+
+        points.text = counter.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Kickball();
+
+        }
+        CheckBallPosition();
+    }
+
+
+    void Kickball()
+    {
+        Vector3 angleX = this.transform.forward;
+        GetComponent<Rigidbody>().AddForce(new Vector3(-angleX.x * power + Random.Range(-10, 0), angleX.y * power + Random.Range(0, 10), Random.Range(0, 1)), ForceMode.Impulse);
+        // GetComponent<Rigidbody>().velocity = new Vector3(-angleX.x * power + Random.Range(-10, 0), angleX.y * power + Random.Range(0, 10), Random.Range(0, 1));
+    }
+
+
+    void CheckBallPosition()
+    {
+        if (GetComponent<Rigidbody>().transform.position.x < -5f || GetComponent<Rigidbody>().transform.position.x > 5f || GetComponent<Rigidbody>().transform.position.z < -5f || GetComponent<Rigidbody>().transform.position.z > 5f)
+        {
+            DecreaseScore();
+
+            //GetComponent<Rigidbody>().velocity = new Vector3(0f,0f,0f);
+            ResetBall();
+            
+            
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Rete"))
+        {
+            counter++;
+            points.text = counter.ToString();
+            ResetBall();
+        }
+
+
+    }
+
+    void DecreaseScore()
+    {
+        if (counter >= 0)
+        {
+            counter--;
+            points.text = counter.ToString();
+        }
+    }
+
+
+    void ResetBall()
+    {
+        GetComponent<Rigidbody>().Sleep();
+        transform.position = new Vector3(-0.67f, 0.09f, 0f);
+        //GetComponent<Rigidbody>().AddForce(new Vector3(0f, 0f, 0f), ForceMode.VelocityChange);
+    }
+
+}
+
+
---
+++ Assets/Scripts/PortiereController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortiereController : MonoBehaviour
+{
+
+    Rigidbody rb;
+    float newPosition;
+    bool adder = true;
+    float fps = 5f;
+    float maxValue = 0.75f;
+    float minValue = -0.75f;
+
+   
+   // Start is called before the first frame update
+   void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        newPosition = 0f;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        //float newPosition = Random.Range(-0.75f, 0.75f);
+
+        /*
+        if (rb.transform.position.z > 0.75f)
+        {
+            newPosition = -0.75f;
+            // newPosition -= (0.01f / 100);
+        }
+        else if (rb.transform.position.z < -0.75f)
+            newPosition = 0f;
+        else
+            newPosition += (0.01f / 100);*/
+        if (adder)
+            newPosition += (0.1f / fps);
+        else
+            newPosition -= (0.1f / fps);
+
+        if (newPosition > maxValue)
+            adder = false;
+        if (newPosition < minValue)
+            adder = true;
+
+        //transform.Translate(new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z * Random.value));
+        rb.transform.position = new Vector3(rb.transform.position.x , rb.transform.position.y , newPosition );
+
+        
+    }
+
+   
+}
